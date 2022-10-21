0716f25fd87fffef3c6081538d6e520cf8dd3fce
Delnat Wito <wito.delnat@gmail.com>
2022-10-21 16:24:15 +0200

    fix: use correct binary

--- Formula/monokle-cli.rb
+++ Formula/monokle-cli.rb
@@ -1,10 +1,11 @@
 class MonokleCli < Formula
   desc "Monokle CLI analyzes your Kubernetes resources to quickly find misconfigurations."
   homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
-  url "https://github.com/kubeshop/monokle-core/releases/download/cli-v0.1.0/monokle.tar.gz"
-  sha256 "e743d0181c2b43d082e0b426f5b632da8b1629799840b5f98cb470bcdb638e9b"
   license "MIT"
+  
   version "0.1.0"
+  url "https://github.com/kubeshop/monokle-core/releases/download/cli-v0.1.0/monokle.tar.gz"
+  sha256 "cbd7311851ecc690cf7aa8fd67806f8376e43366e51d53d399a0b95a7124a417"
 
   def install
     bin.install "monokle"
