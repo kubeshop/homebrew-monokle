871c71569072bad84e4d8d7957301577b363479d
Delnat Wito <wito.delnat@gmail.com>
2022-10-21 16:10:54 +0200

    fix: fix formula

--- Formula/monokle-cli.rb
+++ Formula/monokle-cli.rb
@@ -1,5 +1,5 @@
-class monokle < Formula
-  desc "Monokle cLI analyzes your Kubernetes resources to quickly find misconfigurations."
+class Monoklecli < Formula
+  desc "Monokle CLI analyzes your Kubernetes resources to quickly find misconfigurations."
   homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
   url "https://github.com/kubeshop/monokle-core/releases/download/cli-v0.1.0/monokle.tar.gz"
   sha256 "e743d0181c2b43d082e0b426f5b632da8b1629799840b5f98cb470bcdb638e9b"
