6892a5210e8e8917df4ea173de2e66894575f42b
Delnat Wito <wito.delnat@gmail.com>
2022-10-21 16:07:01 +0200

    add monokle formula for CLI

---
+++ Formula/monokle.rb
@@ -0,0 +1,12 @@
+class monokle < Formula
+  desc "Monokle cLI analyzes your Kubernetes resources to quickly find misconfigurations."
+  homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
+  url "https://github.com/kubeshop/monokle-core/releases/download/cli-v0.1.0/monokle.tar.gz"
+  sha256 "e743d0181c2b43d082e0b426f5b632da8b1629799840b5f98cb470bcdb638e9b"
+  license "MIT"
+  version "0.1.0"
+
+  def install
+    bin.install "monokle"
+  end
+end
