7be45f957a3611ce1f7b96d816ffad89a69c66ea
Hamid <33328203+emamihe@users.noreply.github.com>
2023-03-17 15:51:31 +0300

    Updated monokle-cli.rb

--- Formula/monokle-cli.rb
+++ Formula/monokle-cli.rb
@@ -2,10 +2,15 @@ class MonokleCli < Formula
   desc "Monokle CLI analyzes your Kubernetes resources to quickly find misconfigurations."
   homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
   license "MIT"
-  
-  version "0.3.2"
-  url "https://github.com/kubeshop/monokle-core/releases/download/monokle%2Fcli%400.3.2/monokle.tar.gz"
-  sha256 "59d75d139f0d950c660f2c360cf3a060ebf5aaa5adfc2fe16cb0782e561f60c6"
+
+  version "1.0.7"
+  if Hardware::CPU.arm?
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v1.0.7/monokle-arm.tar.gz"
+    sha256 "1617409eecc38ce44b86e1fd4fd535c2c3b51aad25804eb078dffbd889c6daa5"
+  else
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v1.0.7/monokle-amd64.tar.gz"
+    sha256 "df7f53721a28e4d5a63713c2dc350d4ec6876a92e73537b7cc51c5852bbec303"
+  end
 
   def install
     bin.install "monokle"
