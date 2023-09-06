9dbc77a17fbdbc9426dd924e117e547333bb1959
Hamid <33328203+emamihe@users.noreply.github.com>
2023-09-06 17:41:50 +0300

    Updated monokle-cli.rb

--- Formula/monokle-cli.rb
+++ Formula/monokle-cli.rb
@@ -1,15 +1,15 @@
 class MonokleCli < Formula
   desc "Monokle CLI analyzes your Kubernetes resources to quickly find misconfigurations."
-  homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
+  homepage "https://github.com/kubeshop/monokle-cli"
   license "MIT"
 
-  version "0.5.0"
+  version "0.5.1"
   if Hardware::CPU.arm?
-    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.5.0/monokle-arm.tar.gz"
-    sha256 "135876edec4017951f9ad7c00da70bdf3b2d68e99cddf14053baed5e92e7825b"
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.5.1/monokle-arm.tar.gz"
+    sha256 "2afbe6719de6e54e19a5e897c26c1758ebef6d517c1b2cac633df68423c159f1"
   else
-    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.5.0/monokle-amd64.tar.gz"
-    sha256 "16cf32081a4bf41ceda52d194a4acd260a470dbc0eea159a65620a6f61c49ec0"
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.5.1/monokle-amd64.tar.gz"
+    sha256 "cab128bba6fe09e5ca524924df990a5042c22bbcc403ce41fad540f372bb4873"
   end
 
   def install
