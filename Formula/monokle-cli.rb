6fa89d9f0300d405b92fa3fdee1d2f0d5b6cddd5
Hamid <33328203+emamihe@users.noreply.github.com>
2023-10-20 13:41:12 +0300

    Updated monokle-cli.rb

--- Formula/monokle-cli.rb
+++ Formula/monokle-cli.rb
@@ -3,13 +3,13 @@ class MonokleCli < Formula
   homepage "https://github.com/kubeshop/monokle-cli"
   license "MIT"
 
-  version "0.7.1"
+  version "0.7.2"
   if Hardware::CPU.arm?
-    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.7.1/monokle-arm.tar.gz"
-    sha256 "80d1ec08aa377080df2745d9a1cfd5fb4cf6d1bb924a79b205a30b5f9d8abb9f"
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.7.2/monokle-arm.tar.gz"
+    sha256 "2a5e5823cef2488d157e8979be99c62a025bafa6b9f044f21a9b4c2cb87436d7"
   else
-    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.7.1/monokle-amd64.tar.gz"
-    sha256 "b10218acde5afa4e571ddb19ff4762b48b7778fe3d2dfb395fb9ebeefb233121"
+    url "https://github.com/kubeshop/monokle-cli/releases/download/v0.7.2/monokle-amd64.tar.gz"
+    sha256 "808ce395997a881ab25c776406327de9957c4fa48270db906fce4a86d9204f6c"
   end
 
   def install
